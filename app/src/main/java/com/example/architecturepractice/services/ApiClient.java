96b6c11c57af549b390508a3e6967ad37377b279
Muhamad Ihsan Nuril Anwar <muhamadihsan2020@mail.ugm.ac.id>
2022-05-28 21:23:44 +0700

    ArchitecturePractice - Ihsan

---
+++ app/src/main/java/com/example/architecturepractice/services/ApiClient.java
@@ -0,0 +1,20 @@
+package com.example.architecturepractice.services;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static Retrofit retrofit;
+    public final static String BASE_URL = "https://dog.ceo/api/";
+
+    public static Retrofit getRetrofitInstance() {
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
